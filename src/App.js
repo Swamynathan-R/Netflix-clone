64ff35035b63d054c0c925e19129d1fcba715811
saravana <saravanadale@gmail.com>
2021-10-15 16:39:06 +0530

    completed

---
+++ src/App.js
@@ -0,0 +1,28 @@
+// import axios from './axios';
+import Row from './Row';
+import Nav from './Nav';
+import Banner from './Banner';
+import requests from './requests';
+import './App.css'
+
+
+function App() {
+
+  
+  return (
+    <div className="app">
+    <Nav/>
+    <Banner/>
+    <Row title={"NETFLIX ORIGINALS"} fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true}/>
+    <Row title={"Trending Now"} fetchUrl={requests.fetchTrending}/>    
+    <Row title={"Top Rated"} fetchUrl={requests.fetchTopRated}/>    
+    <Row title={"Action Movies"} fetchUrl={requests.fetchActionMovies}/>    
+    <Row title={"Comedy Movies"} fetchUrl={requests.fetchComedyMovies}/>    
+    <Row title={"Horror Movies"} fetchUrl={requests.fetchHorrorMovies}/>    
+    <Row title={"Romance Movies"} fetchUrl={requests.fetchRomanceMovies}/>    
+    <Row title={"Documentries"} fetchUrl={requests.fetchDocumentries}/>    
+    </div>
+  );
+}
+
+export default App;
